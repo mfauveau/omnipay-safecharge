2a7662855088ba0949f95c76448cef215e072500
Matthieu Fauveau <mfauveau@gmail.com>
2015-05-21 21:46:14 -0400

    Fix Response.php

--- src/Omnipay/Safecharge/Message/AbstractRequest.php
+++ src/Omnipay/Safecharge/Message/AbstractRequest.php
@@ -34,7 +34,7 @@ abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
         $data['sg_ClientLoginID'] = $this->getUsername();
         $data['sg_ClientPassword'] = $this->getPassword();
         $data['sg_IPAddress'] = '127.0.0.1';
-        $data['sg_ResponseFormat'] = 1;
+        $data['sg_ResponseFormat'] = 4;
         $data['sg_Is3dTrans'] = 0;
         $data['sg_ClientUniqueID'] = (string) time();
 
--- src/Omnipay/Safecharge/Message/Response.php
+++ src/Omnipay/Safecharge/Message/Response.php
@@ -1,29 +1,66 @@
 <?php
+
 namespace Omnipay\Safecharge\Message;
+
 use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\RequestInterface;
+use SimpleXMLElement;
+use stdClass;
+
 /**
- * SafeCharge Response
+ * SafeCharge Response.
  */
 class Response extends AbstractResponse
 {
+    public function __construct(RequestInterface $request, $data)
+    {
+        $this->request = $request;
+
+        if (!class_exists('SimpleXMLElement')) {
+            throw new RuntimeException('Insufficient PHP support: missing SimpleXMLElement class');
+        }
+
+        try {
+            $data = new SimpleXMLElement($data);
+        } catch (Exception $e) {
+            throw new RuntimeException('Failed to parse XML response: '.$e->getMessage());
+        }
+
+        $this->data = $data;
+    }
+
     public function isSuccessful()
     {
+        return $this->data->Status != 'ERROR';
+    }
 
+    public function getTransactionReference()
+    {
+        return $this->getTransactionId();
     }
 
     public function getTransactionId()
     {
+        return ((string) $this->data->TransactionID != '') ? $this->data->TransactionID : null;
+    }
 
+    public function getCode()
+    {
+        return $this->getAuthCode();
+    }
+
+    public function getAuthCode()
+    {
+        return ((string) $this->data->AuthCode != '') ? $this->data->AuthCode : null;
     }
 
     public function getToken()
     {
-
+        return ((string) $this->data->Token != '') ? $this->data->Token : null;
     }
 
     public function getMessage()
     {
-
+        return ((string) $this->data->Reason != '') ? $this->data->Reason : null;
     }
 }
